1822a660e00431902b95032529759be366aab0d1
litianzhi <979638192@qq.com>
2018-03-23 22:00:46 +0800

    first

---
+++ guestbook/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$_CONFIGS = array(
+
+	'db'	=>	array(
+		'db_host'		=>	'localhost',
+		'db_port'		=>	'3306',
+		'db_user'		=>	'root',
+		'db_password'	=>	'',
+		'db_name'		=>	'guestbook',
+	),
+
+);
